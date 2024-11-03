1973d31cc10a4f70196143737d219b3ce30b2db5
umerk24 <umerabdkhan24@gmail.com>
2024-11-03 13:59:08 +0500

    component access

--- test.cpp
+++ test.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main() {
     Vector3D obj1(3.0, 4.0, 5.0);
-    cout << obj1.vec[0];
+    cout << obj1.getx();
     return 0;
 }
--- vec3.h
+++ vec3.h
@@ -6,9 +6,10 @@ using namespace std;
 #include <iostream>
 
 class Vector3D{
+    private:
+        double vec[3];
 //constructors
     public:
-        double vec[3];
         Vector3D() {
             vec[0] = 0; vec[1] = 0; vec[2] = 0;
         }
@@ -16,6 +17,15 @@ class Vector3D{
             vec[0] = x; vec[1] = y; vec[2] = z;
         }
 //component access
+    double getx() const{
+        return vec[0];
+    }
+    double gety() const{
+        return vec[1];
+    }
+    double getz() const{
+        return vec[2];
+    }
 
 };
 #endif
