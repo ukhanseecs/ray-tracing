a18c4b19199ac3e715014f56c69bb65f7abc7809
ukhanseecs <ukhan.bee21seecs@seecs.edu.pk>
2025-03-14 14:44:16 +0500

    delete files

--- run.bat
+++
@@ -1,8 +0,0 @@
-   @echo off
-   g++ raytracing.cpp -o raytracing.exe
-   if %errorlevel% neq 0 (
-       echo Compilation failed.
-       exit /b
-   )
-   raytracing.exe > image.ppm
-   echo Image generated: image.ppm
