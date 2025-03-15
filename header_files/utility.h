01f4f9fee7252878532427dbe207b7b466fb0486
ukhanseecs <ukhan.bee21seecs@seecs.edu.pk>
2025-03-15 10:50:12 +0500

    random number utiity

--- header_files/utility.h
+++ header_files/utility.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <random>
 
 using std::make_shared;
 using std::shared_ptr;
@@ -14,6 +15,18 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    // Returns a random real in [0,1).
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0); // Random double between 0 and 1
+    static std::mt19937 generator; // Mersenne Twister pseudo-random generator
+    return distribution(generator); // Return random double
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
 }
 
 #include "ray.h"
