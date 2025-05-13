ec3ca75255755addf7e7b6e4337c223d544c71c4
ukhanseecs <ukhan.bee21seecs@seecs.edu.pk>
2025-05-13 06:34:56 +0500

    Adds Lambertian material class with scattering logic
    Introduces a Lambertian material subclass to handle diffuse
    scattering. Implements a scatter method that calculates the
    scattered ray and attenuation based on the material's albedo.
    
    Enhances material system extensibility.

--- header_files/material.h
+++ header_files/material.h
@@ -14,4 +14,22 @@ class material {
     }
 };
 
+
+class lambertian : public material {
+    private:
+        color albedo; // Albedo (diffuse color) of the material
+    public:
+        lambertian(const color& albedo) : albedo(albedo) {}
+
+        // Scatter function for Lambertian material
+        bool scatter(const Ray& r_in, const hit_record& rec, color& attenuation, Ray& scattered)
+        const override {
+            auto scatter_direction = rec.normal + Vector3D::random_unit_vector();
+            scattered = Ray(rec.p, scatter_direction);
+            attenuation = albedo;
+            return true;
+        }
+
+};
+
 #endif
